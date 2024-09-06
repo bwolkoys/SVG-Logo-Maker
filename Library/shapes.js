64b5b201135971fa34194b8096652ec4e8e01480
Bradyn Wolkoys <bnwolkoys@gmail.com>
2024-09-06 09:31:57 -0700

    added logo.svg file and made prompt to index.js

--- Library/shapes.js
+++ Library/shapes.js
@@ -6,19 +6,21 @@ class Shape{
         this.color=(color);
     }
 };
-
+//Circle
 class Circle extends Shape {
     render () {
         return `<circle cx="150" cy="100" r="80" fill="${this.color}"/>`
     }
 };
 
+//Triangle
 class Triangle extends Shape {
     render () {
         return `<polygon height="100%" width="100%" points="200,50 70,275 330,275" fill="${this.color}"/>`
     }
 };
 
+//Square
 class Square extends Shape {
     render () {
         return `<rect width="400" height="400" fill="${this.color}"/>`
--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { Triangle, Square, Circle } = require("./Library/shapes"); 
+const { Circle, Triangle, Square } = require("./Library/shapes"); 
+const { error } = require('console');
+
+inquirer
+    .promt ([
+        {
+            type: 'input',
+            message: 'enter up to three characters',
+            name: 'characters',
+        },
+        {
+            type: 'input',
+            message: 'enter the text color',
+            name: 'textColor', 
+        },
+        {
+            type: 'list',
+            message: 'pick a shape below',
+            choices: [{name: 'Circle', name: 'Triangle', name: 'Square'}],
+            name: 'shape',
+        },
+        {
+            type: 'input',
+            message: 'enter the shape color',
+            name: 'shapeColor', 
+        }
+    ])
+    
+    .then((response) => {
+        if(response.shape === 'Circle') {
+            const square = new Square(response.characters, response.textColor, response.shapeColor)
+            fs.writeFile('./Examples/Logo.svg', circle.render(), (error) => {
+                if(error) {
+                    console.log(error);
+                }
+            });
+        }
+    })
