2d63520ac1218206dbdaad9c23ed620750ee91ac
Bradyn Wolkoys <bnwolkoys@gmail.com>
2024-09-06 09:17:11 -0700

    added code to shapes.js page

---
+++ Library/shapes.js
@@ -0,0 +1,28 @@
+class Shape{
+    constructor(){
+        this.color=''
+    }
+    setColor(color){
+        this.color=(color);
+    }
+};
+
+class Circle extends Shape {
+    render () {
+        return `<circle cx="150" cy="100" r="80" fill="${this.color}"/>`
+    }
+};
+
+class Triangle extends Shape {
+    render () {
+        return `<polygon height="100%" width="100%" points="200,50 70,275 330,275" fill="${this.color}"/>`
+    }
+};
+
+class Square extends Shape {
+    render () {
+        return `<rect width="400" height="400" fill="${this.color}"/>`
+    }
+};
+
+module.exports = {Circle, Triangle, Square}
